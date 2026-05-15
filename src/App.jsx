abd41b6dded37c88445ea7fafadf0a3fda400af6
mostakinahmed <mostakin4556@gmail.com>
2026-05-15 12:17:52 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import './index.css'
+
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-indigo-600">
+      <div className="rounded-2xl bg-white p-12 shadow-2xl">
+        <h1 className="text-4xl font-black text-indigo-600 uppercase tracking-widest">
+          Tailwind v4 Active!
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
